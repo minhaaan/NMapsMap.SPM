c8cf233e605dfc0fa63a4bf164ac68d31e1e525f
minhaaan <chmh0411@gmail.com>
2023-10-29 17:34:37 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     .binaryTarget(
       name: "NMapsGeometry",
       url: "https://github.com/minhaaan/NMapsGeometry/releases/download/1.0.1/NMapsGeometry.xcframework.zip",
-      checksum: "a39db59cf754cffbaed769b74e6823313581c1d639998accac5ce87991ae24c5"
+      checksum: "ca57d5ccef5e3855078c8237b46c937f522f155c7a51bd17cb1729a36e37b596"
     )
   ]
 )
